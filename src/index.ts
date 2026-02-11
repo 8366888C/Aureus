51da07c1c11b8befcb95dc84b003a920493ec20b
Rahul <deploy.rahul@gmail.com>
2026-02-11 13:26:47 +0530

    feat: add 'view' and 'add' options for gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,104 @@
 import { Command } from "commander";
+import pc from "picocolors";
 import Enquirer from "enquirer";
 const { prompt } = Enquirer;
 import path from "node:path";
 import fs from "node:fs";
+import { getGitignoreTemplate, GIChoices } from "./commands/gitignore.js";
 
 const program = new Command();
 program
   .name(process.env.NAME as string)
-  .version(process.env.VERSION as string)
-  .description(process.env.DESCRIPTION as string);
+  .description(process.env.DESCRIPTION as string)
+  .version(process.env.VERSION as string, "-v, --version");
 
-program.action(async () => {
-  const response = await prompt<{ name: string; language: string }>([
-    {
-      type: "input",
-      name: "name",
-      message: "what is your name ?",
-      initial: "world",
-    },
-    {
-      type: "select",
-      name: "language",
-      message: "choose your preferred language",
-      choices: ["english", "hindi", "japanese"],
-    },
-  ]);
-  const { name, language } = response;
+// ! main INIT command
+program
+  .command("init")
+  .description("Initialize a new project")
+  .argument(
+    "[folder]",
+    "Folder name to create (use . for current directory)",
+    ".",
+  )
+  .action(async (folder) => {
+    console.log(pc.green(`Initializing project in: ${folder}`));
+  });
 
-  let greeting = "";
-  switch (language) {
-    case "hindi":
-      greeting = `namaste ${name}`;
-    case "japanese":
-      greeting = `konnichiwa ${name}`;
-    default:
-      greeting = `hello ${name}`;
-  }
+// ! ADD subcommand group
+const add = program
+  .command("add")
+  .description("Add specific repository standards");
 
-  const filePath = path.join(process.cwd(), "greetings.txt");
-  fs.writeFileSync(filePath, greeting);
-});
+add
+  .command("license")
+  .description("Add a LICENSE file")
+  .action(() => {});
+
+add
+  .command("gitignore")
+  .description("Add a template .gitignore file")
+  .action(async () => {
+    const response = await prompt<{
+      gitignore: string;
+    }>({
+      type: "autocomplete",
+      name: "gitignore",
+      message: "",
+      choices: GIChoices,
+    });
+
+    try {
+      const text = getGitignoreTemplate(response.gitignore);
+      fs.writeFileSync(path.join(process.cwd(), ".txt"), text);
+      console.log(
+        `Successfully generated .gitignore for ${response.gitignore}`,
+      );
+    } catch (e) {
+      console.error(`Cannot create .gitignore : ${e}`);
+    }
+  });
+
+add
+  .command("readme")
+  .description("Generate a template README.md")
+  .action(() => {
+    console.log("Generating README...");
+  });
+
+// ! VIEW subcommand group
+const view = program.command("view").description("View specific templates");
+
+view
+  .command("license")
+  .description("Add a LICENSE file")
+  .action(() => {});
+
+view
+  .command("gitignore")
+  .description("View a template .gitignore file")
+  .action(async () => {
+    const response = await prompt<{
+      gitignore: { name: string; value: string };
+    }>({
+      type: "autocomplete",
+      name: "gitignore",
+      message: "",
+      choices: GIChoices,
+    });
+  });
+
+view
+  .command("readme")
+  .description("Generate a template README.md")
+  .action(() => {
+    console.log("Generating README...");
+  });
+
+// ! END
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+  process.exit(0);
+}
 
 program.parse(process.argv);
